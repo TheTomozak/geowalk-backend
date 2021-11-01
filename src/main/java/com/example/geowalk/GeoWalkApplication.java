3db136c5c6518fed93cc8348974c1b7c4dc15c04
Lukasz <lukasznguyen1999@gmail.com>
2021-11-01 21:52:58 +0100

    Generated project in Spring Initializr

---
+++ src/main/java/com/example/geowalk/GeoWalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.geowalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geowalk/GeoWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geowalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
