b343b6c09968d9886be22f29c9b5dacc4e3912a7
s18969 <tomaszpalczewski99@gmail.com>
2021-11-04 18:43:07 +0100

    lukasz

---
+++ src/main/java/com/example/geowalk/models/dto/responses/UserResDto.java
@@ -0,0 +1,62 @@
+package com.example.geowalk.models.dto.responses;
+
+import com.example.geowalk.models.enums.Role;
+
+public class UserResDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+
+    public UserResDto() {
+    }
+
+    public UserResDto(String firstName, String lastName, String email, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
