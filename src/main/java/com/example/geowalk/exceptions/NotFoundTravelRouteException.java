d00902c66a5c6e2b33fc9415084052605ce855fd
mateuszsuchecki <s19130@pjwstk.edu.pl>
2021-11-21 18:05:53 +0100

    Usuniecie nie potrzebnych klas

--- src/main/java/com/example/geowalk/exceptions/NotFoundTravelRouteException.java
+++
@@ -1,7 +0,0 @@
-package com.example.geowalk.exceptions;
-
-public class NotFoundTravelRouteException extends RuntimeException {
-    public NotFoundTravelRouteException(String message) {
-        super(message);
-    }
-}
